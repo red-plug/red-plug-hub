30f7077c9d4bbbe2cb55a727d06d1f6c63089d5b
andreslqr <me@andreslopez.com.mx>
2025-05-02 20:12:41 -0600

    changed name

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="logo-container">
     <img src="https://redplug.com.mx/logo/red-plug-responsive.svg" height="30" width="30" alt="Red Plug Logo" />
-    <h3 class="logo-text">Red Plug</h3>
+    <h3 class="logo-text">Red Hub</h3>
   </div>
 </template>
